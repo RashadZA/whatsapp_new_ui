56bb1cf5aea7a993839f52375e6181fe13fc4890
RashadZA <97411218+RashadZA@users.noreply.github.com>
2024-01-27 21:56:09 +0600

    Added some const, global, and theme files

---
+++ lib/features/app/const/consts.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+import 'package:fluttertoast/fluttertoast.dart';
+import 'package:giphy_picker/giphy_picker.dart';
+import 'package:whatsapp_new_ui/features/app/theme/style.dart';
+
+part 'agora_config_const.dart';
+part 'app_const.dart';
+part 'firebase_collection_const.dart';
+part 'message_type_const.dart';
+part 'page_const.dart';
---
+++ lib/features/app/const/message_type_const.dart
@@ -0,0 +1,12 @@
+part of 'consts.dart';
+
+class MessageTypeConst {
+
+  static const String textMessage = "textMessage";
+  static const String fileMessage = "fileMessage";
+  static const String emojiMessage = "emojiMessage";
+  static const String photoMessage = "photoMessage";
+  static const String audioMessage = "audioMessage";
+  static const String videoMessage = "videoMessage";
+  static const String gifMessage = "gifMessage";
+}
