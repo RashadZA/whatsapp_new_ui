466b1ea69244964c980990072f9fcf493d2ddf0a
RashadZA <97411218+RashadZA@users.noreply.github.com>
2024-01-28 21:36:30 +0600

    Injected Dependency for User feature

--- lib/main.dart
+++ lib/main.dart
@@ -2,8 +2,10 @@ import 'package:flutter/material.dart';
 import 'package:whatsapp_new_ui/features/app/splash/splash_screen.dart';
 import 'package:whatsapp_new_ui/features/app/theme/app_theme.dart';
 import 'package:whatsapp_new_ui/routes/on_generate_routes.dart';
+import 'main_injection_container.dart' as di;
 
-void main() {
+Future<void> main() async {
+  // await di.init();
   runApp(const MyApp());
 }
 
