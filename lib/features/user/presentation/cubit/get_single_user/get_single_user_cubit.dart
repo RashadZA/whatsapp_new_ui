5f388c2d8da49242221c9961f6c69d79715cbe58
RashadZA <97411218+RashadZA@users.noreply.github.com>
2024-01-28 21:18:25 +0600

    Added bloc cubit file in  User Presentation Layer.

---
+++ lib/features/user/presentation/cubit/get_single_user/get_single_user_cubit.dart
@@ -0,0 +1,22 @@
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:equatable/equatable.dart';
+import 'package:whatsapp_new_ui/features/user/domain/usecases/user/get_single_user_usecase.dart';
+
+import '../../../domain/entities/user_entity.dart';
+
+part 'get_single_user_state.dart';
+
+class GetSingleUserCubit extends Cubit<GetSingleUserState> {
+  final GetSingleUserUseCase getSingleUserUseCase;
+  GetSingleUserCubit({
+    required this.getSingleUserUseCase
+}) : super(GetSingleUserInitial());
+
+  Future<void> getSingleUser({required String uid}) async {
+    emit(GetSingleUserLoading());
+    final streamResponse = getSingleUserUseCase.call(uid);
+    streamResponse.listen((users) {
+      emit(GetSingleUserLoaded(singleUser: users.first));
+    });
+  }
+}
